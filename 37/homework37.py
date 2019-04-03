c1d5117d6723cf16f4a754a2aea635f8f1ae03ef
clubart <cmlubart@gmail.com>
2019-04-03 10:47:39 -0400

    homework37

---
+++ 37/homework37.py
@@ -0,0 +1,9 @@
+names = ['Chaim Lubart', 'Yosef Lubart', 'Moishy Lubart']
+address = '217 Coles way'
+print(f'''name: {names[0]} address: {address}
+name: {names[1]} address: {address}
+name: {names[2]} address: {address}
+''')
+
+print(names[0][::3])
+print(names[-2][1:-1])
