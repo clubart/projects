0c26945c883c44a9f21aab6afa66b4c6a6c82328
clubart <cmlubart@gmail.com>
2019-04-04 20:44:37 -0400

    homework 38

---
+++ python/class/class37/app.py
@@ -0,0 +1,17 @@
+print("Hello World")
+x = 5
+print(x)
+x = 'Hello'
+print(x, type(x))
+y = 4.4
+print(y, type(y))
+z = True
+print(z, type(z))
+print(id(z))
+
+students = ['Donald', 'Mike', 'Jared', 'Ivanka', 'Melania']
+print(students, type(students))
+print(students[0][1:6:2])
+print(students[1:4:2])
+numbers = [1, 2, 3, 4, 5]
+print(numbers * 2)
