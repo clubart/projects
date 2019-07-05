fcade9824c23f1c03b6fe3c029c83ab03d79e3cb
clubart <cmlubart@gmail.com>
2019-07-04 22:25:20 -0400

    test revised

--- javascript/homework/58/count.js
+++ javascript/homework/58/count.js
@@ -9,11 +9,11 @@
     for (let i = 0; i < 5; i++) {
         counter1.counter();
     }
-    console.log('counter1 is at ', counter1.getCurrentCount());
 
     const counter2 = app.createCounter();
     for (let i = 0; i < 15; i++) {
         counter2.counter();
     }
+    console.log('counter1 is at ', counter1.getCurrentCount());
     console.log('counter2 is at ', counter2.getCurrentCount());
 }());
--- javascript/homework/58/createCounter.js
+++ javascript/homework/58/createCounter.js
@@ -1,9 +1,10 @@
 var app = app || {};
 app = (function () {
     'use strict';
-
-
+    let numOfCounters = 0;
     app.createCounter = function () {
+        numOfCounters++;
+        console.log('number of counters', numOfCounters);
         let count = 0;
         return {
             counter: function () {
@@ -11,6 +12,9 @@ app = (function () {
             },
             getCurrentCount: function () {
                 return count;
+            },
+            getNumOfCounters: function () {
+                console.log(numOfCounters);
             }
         };
     };
