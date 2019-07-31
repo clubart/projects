fdadb79e65abcef53a6c58ca29b55e56738f47ae
clubart <cmlubart@gmail.com>
2019-07-30 22:01:30 -0400

    homework64

---
+++ javascript/homework/64/hw.js
@@ -0,0 +1,33 @@
+/* global $ */
+(function () {
+    'use strict';
+
+    $('#loadButton').click(() => {
+        const userInput = $('#fileInput').val();
+        const mySpinner = $('<img src="spinner.png"></img>')
+            .prependTo($(document.body))
+            .css('position', 'fixed')
+            .css('top', '50')
+            .css('right', '50');
+        setTimeout(() => {
+            fetch(userInput)
+                .then(r => {
+                    if (r.ok) {
+                        return r.text();
+                    } else {
+                        throw new Error(r.status);
+                    }
+                })
+                .then(rt => {
+                    $('#myPlaceholder').text(rt);
+                    //$('body').append(rt);
+                    //$('body').text(rt);
+                    mySpinner.hide();
+                })
+                .catch(() => {
+                    mySpinner.hide();
+                    window.pcs.messageBox.show('could not find file');
+                });
+        }, 2000);
+    });
+}());
