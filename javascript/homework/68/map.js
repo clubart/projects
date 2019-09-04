87bb758350e6e08c2d91c16479f135ac33872f22
clubart <cmlubart@gmail.com>
2019-09-03 23:30:14 -0400

    homework68

---
+++ javascript/homework/68/map.js
@@ -0,0 +1,52 @@
+/* global $, google */
+(function () {
+    'use strict';
+
+    const searchButton = $('#searchButton');
+    const userInput = $('#userInput');
+    const results = $('#results');
+    let map;
+
+
+    function createMap() {
+        map = new google.maps.Map(document.getElementById('showMap'), {
+            center: { lat: 40.096068191437205, lng: -74.222168870743332 },
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.SATELLITE
+        });
+    }
+
+    function createMarkers(city) {
+        let marker = new google.maps.Marker({
+            position: { lat: city.lat, lng: city.lng },
+            map: map,
+            title: city.title,
+        });
+        let infoWindow = new google.maps.InfoWindow();
+        marker.addListener('click', () => {
+            infoWindow.setContent(city.summary);
+            infoWindow.open(map, marker);
+        });
+    }
+
+    searchButton.click(() => {
+        fetch(`http://api.geonames.org/wikipediaSearch?q=${userInput.val()}&maxRows=10&username=clubart&type=json`)
+            .then(r => {
+                if (r.ok) {
+                    return r.json();
+                } else {
+                    throw new Error(r.status);
+                }
+            })
+            .then(cities => {
+                createMap();
+                cities.geonames.forEach(city => {
+                    results.append(`<h4>${city.title}</h4>`)
+                        .click(rt => {
+                            
+                        });
+                    createMarkers(city);
+                });
+            });
+    });
+}());
